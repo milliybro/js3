b63ba550d362e594c672cd574eb3305604973e84
“milliybro” <shohrux-rustamov@mail.ru>
2023-07-31 11:46:34 +0500

    first commit

--- case6x.js
+++ case6x.js
@@ -1,1 +1,97 @@
 `Case6. Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (oy). (Kabisa bo'lmagan yil sanasi kiritiladi). Berilgan sanani ifodalovchi programma tuzilsin. Kabisa yilida 366 kun, kabisa bo'lmagan yilda 365 kun bor bo'ladi.`
+
+
+ab = 27;
+M = 6;
+
+a = parseInt(ab / 10);
+b = ab % 10;
+
+switch (a) {
+  case 1:
+    console.log(`O'n`);
+    break;
+  case 2:
+    console.log(`Yigirma`);
+    break;
+  case 3:
+    console.log(`O'ttiz`);
+    break;
+
+  default:
+    break;
+}
+switch (b) {
+  case 1:
+    console.log(`birinchi`);
+    break;
+  case 2:
+    console.log(`ikkinchi`);
+    break;
+  case 3:
+    console.log(`uchinchi`);
+    break;
+  case 4:
+    console.log(`to'rtinchi`);
+    break;
+  case 5:
+    console.log(`beshinchi`);
+    break;
+  case 6:
+    console.log(`oltinchi`);
+    break;
+  case 7:
+    console.log(`yettinchi`);
+    break;
+  case 8:
+    console.log(`sakkizinchi`);
+    break;
+  case 9:
+    console.log(`toqqizinchi`);
+    break;
+
+  default:
+    break;
+}
+
+switch (M) {
+  case 1:
+    console.log(`Yanvar`);
+    break;
+  case 2:
+    console.log(`Fevral`);
+    break;
+  case 3:
+    console.log(`Mart`);
+    break;
+  case 4:
+    console.log(`Aprel`);
+    break;
+  case 5:
+    console.log(`May`);
+    break;
+  case 6:
+    console.log(`Iyun`);
+    break;
+  case 7:
+    console.log(`Iyul`);
+    break;
+  case 8:
+    console.log(`Avgust`);
+    break;
+  case 9:
+    console.log(`Sentabr`);
+    break;
+  case 10:
+    console.log(`Oktabr`);
+    break;
+  case 11:
+    console.log(`Noyabr`);
+    break;
+  case 12:
+    console.log(`Dekabr`);
+    break;
+
+  default:
+    break;
+}
